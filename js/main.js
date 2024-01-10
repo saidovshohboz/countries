52a737e6a6b85c90543f6dc7f569ab59e88a6d46
saidovshohboz <shohbozsaidovv@gmail.com>
2024-01-10 12:19:48 +0500

    Toggle dark-theme on site-header-toggler click

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let elSiteThemeToggler = document.querySelector(".site-header-toggler");
+
+elSiteThemeToggler.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
